58bdb66ae3fb666b75fe08da15a3f766de58390c
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-07-13 16:19:07 +0530

    recent changes updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-     <div className="bg-fuchsia-700">
+     <div className="bg-blue-900 text-amber-50">
        Hello world
      </div>
   )
