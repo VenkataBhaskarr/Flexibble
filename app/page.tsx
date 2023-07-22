820e97c2df8284ad94d5107131a64e31d38101fe
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-07-22 23:07:31 +0530

    recent changes updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-     <div className="bg-blue-900 text-amber-50">
-       Hello world
-     </div>
+    <div>
+        <section className={"flex-start flex-col paddings mb-16"}>
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>Load More</h1>
+        </section>
+    </div>
   )
 }
